01d2fbdf7bb58b126cec2d8dbce779ca0f66bde7
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-01-25 22:05:15 +0300

    haskell: don't do what others don't do

--- udp-fixed-localhost-flood.hs
+++ udp-fixed-localhost-flood.hs
@@ -10,13 +10,13 @@ import qualified Data.ByteString.Char8 as B8
 toSend :: Int
 toSend = 10 * 1000 * 1000
 
+b :: B8.ByteString
+b = B8.replicate 64 'Q'
+
 main = withSocketsDo $ do
   print toSend
-  let b = B8.replicate 64 'Q'
   addrInfo <- head <$> getAddrInfo (Just defaultHints{addrSocketType=Datagram}) (Just "127.0.0.1") (Just "9999")
   s <- socket (addrFamily addrInfo) Datagram defaultProtocol
   let a = addrAddress addrInfo
-  let loop = sendTo s b a >>= \i -> do
-                when (i==0)  $ throwIO (ErrorCall "fck")
-                when (i/=64) loop
+  let loop = sendTo s b a >>= \i -> when (i==0)  $ throwIO (ErrorCall "fck")
   replicateM_ toSend loop
