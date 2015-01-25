1e73be0e723d79e6dd30eec35d52685550b6ab91
Alexander Vershilov <alexander.vershilov@gmail.com>
2015-01-25 21:37:27 +0300

    haskell: minor fixes

--- udp-fixed-localhost-flood.hs
+++ udp-fixed-localhost-flood.hs
@@ -14,10 +14,9 @@ main = withSocketsDo $ do
   print toSend
   let b = B8.replicate 64 'Q'
   addrInfo <- head <$> getAddrInfo (Just defaultHints{addrSocketType=Datagram}) (Just "127.0.0.1") (Just "9999")
-  print addrInfo
   s <- socket (addrFamily addrInfo) Datagram defaultProtocol
-  print $ addrAddress addrInfo
-  let loop = sendTo s b (addrAddress addrInfo) >>= \i -> do
+  let a = addrAddress addrInfo
+  let loop = sendTo s b a >>= \i -> do
                 when (i==0)  $ throwIO (ErrorCall "fck")
                 when (i/=64) loop
   replicateM toSend $ loop
